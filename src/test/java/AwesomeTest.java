7013ea18ffdf357ac9ffab97bddce56f2e5097ba
Omer Ozdil <omerozdil54@gmail.com>
2020-10-06 20:59:15 +0100

    Added more awesome staff

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -3,4 +3,9 @@ public class AwesomeTest {
     //green means it is under staging area
     // green in .dea folder meas it is ignored
     // when the color is normal, it means it is committed no changes.
+
+    // I just pulled the new change from GitHub that i did not have locally.
+    // and now my project is in sync with remote repository.
+
+    // now I made more awesome changes  and I AM BOUT TO push this back to GitHub.
 }
