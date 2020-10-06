7ec8218521cf43fe75d17efbad47e6c19a5a3b07
Omer Ozdil <omerozdil54@gmail.com>
2020-10-06 20:56:18 +0100

    Added new file for awesome feature

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -2,5 +2,5 @@ public class AwesomeTest {
     // red means test it is untracked
     //green means it is under staging area
     // green in .dea folder meas it is ignored
-    //
+    // when the color is normal, it means it is committed no changes.
 }
